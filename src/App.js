8682e927c894e64e14ffb15eda1b0018a2e5167f
Martin Stoyanov <martinestoyanov@gmail.com>
2021-05-01 15:02:44 -0400

    Working nav bar and formfield

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bulma/css/bulma.css";
+import Navbar from "./navbar/navbar";
+import Formfield from "./formfield/formfield";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Formfield label="Name" type="text" placeholder="e.g Alex Smith" />
+      <Formfield label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
     </div>
   );
 }
+
+
 
 export default App;
